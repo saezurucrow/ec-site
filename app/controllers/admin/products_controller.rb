f3a8e9614ec27347b829cd58b19f771e51628473
saezurucrow <usobukuhito@gmail.com>
2019-07-31 04:11:10 +0000

    フォームの型作成

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -9,17 +9,33 @@ class Admin::ProductsController < ApplicationController
   end
 
   def new
+    @product.new
   end
 
   def search
   end
 
-  def destroy
-  end
-
   def create
+    @product = Product.new(product_params)
+
+    if @product.save
+      flash[:notice] = "投稿成功"
+      redirect_to admin_product_path(@product)
+    else
+      flash[:notice] = "投稿失敗"
+      render :new
+    end
   end
 
   def update
   end
+
+  def destroy
+  end
+
+  private
+
+    def product_params
+        params.require(:product).permit(:name,:price,:jacket,:stock,:product_status)
+    end
 end
