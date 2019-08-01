7ff9b76c20011fdf5ceadffa8061de3661c2d5ea
saezurucrow <usobukuhito@gmail.com>
2019-08-01 08:14:21 +0000

    どこでも検索できるようにした

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
+  before_action :product_search
 
+  private
+
+  def product_search
+    @q = Product.ransack(params[:q])
+    @products = @q.result(distinct: true)
+  end
 end
