086e7989b4daeb60b1024194c41c499f54ef585c
saezurucrow <usobukuhito@gmail.com>
2019-08-05 07:22:23 +0000

    投稿できたよ

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -10,39 +10,26 @@ class Admin::ProductsController < ApplicationController
 
   def new
     @product = Product.new
-    @artist = Artist.new
-    @label = Label.new
-    @genre = Genre.new
+    @artist = Artist.all
+    @label = Label.all
+    @genre = Genre.all
   end
 
   def search
   end
 
-  def create #絶対もっと簡略化できる
+  def create
     @product = Product.new(product_params)
 
-    @artist = Artist.new(artist_params)
-    @label = Label.new(label_params)
-    @genre = Genre.new(genre_params)
-
-    if @artist.save && @label.save && @genre.save
-      @product.artist_id = @artist.id
-      @product.label_id = @label.id
-      @product.genre_id = @genre.id
-
-      if @product.save
-        redirect_to admin_product_path(@product)
-      else
-        flash[:notice] = "投稿失敗"
-        p @product.errors.full_messages
-        render :new
-      end
-
+    if @product.save
+      flash[:notice] = "投稿成功"
+      redirect_to admin_product_path(@product)
     else
       flash[:notice] = "投稿失敗"
       p @product.errors.full_messages
       render :new
     end
+
   end
 
   def update
@@ -55,22 +42,7 @@ class Admin::ProductsController < ApplicationController
 
     def product_params
         params.require(:product).permit(
-          :name,:price,:jacket,:stock,:product_status,
-          artist_attributes: [:id, :name],
-          artist_attributes: [:id, :name],
-          artist_attributes: [:id, :name]
+          :name,:price,:jacket,:stock,:product_status,:artist_id,:label_id,:genre_id
         )
-    end
-
-    def artist_params
-        params.require(:artist).permit(:name)
-    end
-
-    def label_params
-        params.require(:label).permit(:name)
-    end
-
-    def genre_params
-        params.require(:genre).permit(:name)
     end
 end
