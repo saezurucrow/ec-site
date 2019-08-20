3d6b4cb17089aa72509ec8670e58ade69acd960b
saezurucrow <usobukuhito@gmail.com>
2019-08-20 02:59:41 +0000

    ユーザ編集機能追加

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,9 +1,15 @@
 class CustomersController < ApplicationController
   before_action :authenticate_customer!
   def show
+    @customer = Customer.find(params[:id])
   end
 
   def edit
+    @customer = Customer.find(params[:id])
+    if @customer == current_customer
+    else
+      flash[:notice] = "ダメです。"
+    end
   end
 
   def destroy
