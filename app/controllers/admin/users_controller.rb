b35f8715735c9cb6d713d749eb182e1883e67585
saezurucrow <usobukuhito@gmail.com>
2019-08-30 00:57:52 +0000

    user詳細編集機能追加

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -10,17 +10,29 @@ class Admin::UsersController < ApplicationController
   end
 
   def edit
-  end
-
-  def destroy
+    @user = Customer.find(params[:id])
   end
 
   def update
+    @user = Customer.find(params[:id])
+    if @user.update(user_params)
+  	  redirect_to admin_user_path(@user)
+    else
+      render :show
+    end
+  end
+
+  def destroy
   end
 
   private
 
   def admin_params
     params.require(:admin).permit(:name, :mail, :password, :password_confirmation)
+  end
+
+  def user_params
+    params.require(:customer).permit(:last_name,:last_name_kana,:first_name,:first_name_kana,
+                          :image,:postal_code,:address,:tel,:email)
   end
 end
