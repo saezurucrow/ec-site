0513731d5231e83b3a2540c7cdda793dd957e9ee
saezurucrow <usobukuhito@gmail.com>
2019-09-11 07:58:46 +0000

    migration編集

--- db/migrate/20190831032050_add_customer_id_to_carts.rb
+++
@@ -1,5 +0,0 @@
-class AddCustomerIdToCarts < ActiveRecord::Migration[5.2]
-  def change
-    add_column :carts, :customer_id, :integer
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_06_175853) do
+ActiveRecord::Schema.define(version: 2019_09_11_073951) do
 
   create_table "addresses", force: :cascade do |t|
     t.integer "customer_id"
@@ -49,9 +49,9 @@ ActiveRecord::Schema.define(version: 2019_09_06_175853) do
   end
 
   create_table "carts", force: :cascade do |t|
+    t.integer "customer_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "customer_id"
   end
 
   create_table "customers", force: :cascade do |t|
