3d78651698f34c6770ce128a48ed4f5858ee36eb
saezurucrow <usobukuhito@gmail.com>
2019-09-21 12:02:51 +0000

    address編集

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -17,9 +17,19 @@ class AddressesController < ApplicationController
   end
 
   def edit
+    @address = Address.find(params[:id])
+    render root_path if current_customer =! @address.customer
   end
 
   def update
+    @address = Address.find(params[:id])
+    if @address.update(address_params)
+      flash[:notice] = "変更完了しました。"
+  	  redirect_to customer_path(@address.customer)
+    else
+      flash[:notice] = "変更失敗しました。"
+      render :edit
+    end
   end
 
   def destory
