901092f147972e6a5b51c0314507171fa4c1aaaa
saezurucrow <usobukuhito@gmail.com>
2019-10-16 02:25:59 +0000

    migratin変更

--- db/migrate/20190905021609_change_data_iroiro_to_orders.rb
+++ db/migrate/20190905021609_change_data_iroiro_to_orders.rb
@@ -1,6 +1,6 @@
 class ChangeDataIroiroToOrders < ActiveRecord::Migration[5.2]
   def change
-    change_column :orders, :order_status, :integer
+    change_column :orders, :order_status, 'integer USING CAST(order_status AS integer)'
     change_column :orders, :payment, :integer
     change_column :orders, :postal_code, :string
   end
