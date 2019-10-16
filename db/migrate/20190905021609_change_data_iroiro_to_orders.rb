a4511b4bd0a97ff03e5fb5db601911119c37e3f1
saezurucrow <usobukuhito@gmail.com>
2019-10-16 02:30:18 +0000

    migration error 変更

--- db/migrate/20190905021609_change_data_iroiro_to_orders.rb
+++ db/migrate/20190905021609_change_data_iroiro_to_orders.rb
@@ -1,7 +1,7 @@
 class ChangeDataIroiroToOrders < ActiveRecord::Migration[5.2]
   def change
     change_column :orders, :order_status, 'integer USING CAST(order_status AS integer)'
-    change_column :orders, :payment, :integer
+    change_column :orders, :payment, 'integer USING CAST(payment AS integer)'
     change_column :orders, :postal_code, :string
   end
 end
